3cc34eac7e46b313ccb8b252e99af26c5db5a5a8
Sohaib-Abdullah <sohaibsohaib77@gmail.com>
2024-08-30 15:28:13 +0500

    adding-cors-link

--- api/index.js
+++ api/index.js
@@ -31,11 +31,24 @@ mongoose.connection.on("connected", () => {
 });
 
 const corsOptions = {
-  origin: "*",
+  origin: [
+    "https://hotel-booking-app-client-lf3vjiqie-sohaib-abdullahs-projects.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  // preflightContinue: false, // access-control-allow-origin: *
   credentials: true, // access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+// app.use((_req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   next();
+// });
+
 app.use(cookieParser());
 app.use(express.json());
 
